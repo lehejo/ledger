bf56c6a2313d6ffb0dfacfa17a2cfe3c4982ec05
manopickax <rnrrbs0202@gmail.com>
2023-05-15 19:38:14 +0900

    Update Main.java

--- LedgerProject/src/Ledger/Main.java
+++ LedgerProject/src/Ledger/Main.java
@@ -1,7 +1,24 @@
 package Ledger;
 //사용자 입력 및 프로그램 실행을 담당합니다.
-public class Main{
-    public static void main(String[] args){
-        System.out.print("Hello World");
-    }
+import javax.swing.*;
+
+public class Main extends JFrame {
+  public Main() {
+    // JFrame의 기본 설정
+    this.setTitle("가계부 프로그램");
+    this.setSize(1000, 500);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    // JFrame에 컴포넌트 추가
+    JLabel label = new JLabel("안녕하세요!");
+    this.add(label);
+    
+    // JFrame을 화면에 표시
+    this.setVisible(true);
+  }
+  
+  public static void main(String[] args) {
+    // 새로운 Main 객체 생성
+    Main frame = new Main();
+  }
 }
